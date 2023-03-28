d2b733de1fea1926da9d307ea7ac6ce7a2ea9882
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-28 13:34:02 +0900

    Apply suggestions from code review

--- kaggle-notebooks/notebooks/getfem-torsion.py
+++ kaggle-notebooks/notebooks/getfem-torsion.py
@@ -16,15 +16,15 @@
 #
 # 半径方向の単位ベクトル $P$ は以下の式であらわされる。
 #
-# ```math
+# $$
 # P＝\begin{pmatrix}\cos \theta &\sin \theta \end{pmatrix}
-# ```
+# $$
 #
 # ベクトル $P$ を角度 $\theta$ で微分すると接線方向の単位ベクトルを得ることができる。
 #
-# ```math
+# $$
 # \frac{dP}{d\theta }=\begin{pmatrix}-\sin \theta &\cos \theta \end{pmatrix}
-# ```
+# $$
 #
 # このノートはGetFEMの動作確認を行うために作成したものです。
 
