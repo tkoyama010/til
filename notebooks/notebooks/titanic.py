8b18cf6ff786a26b44f8b4d8f94b20048d1c0941
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-29 16:18:57 +0900

    Update titanic.py

--- notebooks/notebooks/titanic.py
+++ notebooks/notebooks/titanic.py
@@ -23,3 +23,53 @@
 # %% [markdown]
 # ダウンロードの際には`kaggle.json`ファイルを `~/.kaggle/kaggle.json` に配置しておく必要があります。
 # `titanic.zip`ファイルがダウンロードされますのでカレントディレクトリに展開します。
+
+# %%
+# !unzip titanic.zip
+
+# %% [markdown]
+# `train.csv` ファイルに訓練用のデータが保存されていますので `pandas.read_csv` で読み込みます。
+
+# %%
+import pandas as pd
+
+train = pd.read_csv("train.csv")
+train.head(3).style.set_table_attributes('style="font-size: 10px"')
+
+# %% [markdown]
+# `test.csv` ファイルにテスト用のデータが保存されていますので同様に `pandas.read_csv` で読み込みます。
+
+# %%
+import pandas as pd
+
+test = pd.read_csv("test.csv")
+test.head(3).style.set_table_attributes('style="font-size: 10px"')
+
+# %% [markdown]
+# また、 `seaborn` というライブラリを使用してもデータを取得することができます。
+
+# %%
+# see https://seaborn.pydata.org/examples/logistic_regression.html
+import seaborn as sns
+
+sns.set_theme(style="darkgrid")
+
+# Load the example Titanic dataset
+df = sns.load_dataset("titanic")
+
+# Make a custom palette with gendered colors
+pal = dict(male="#6495ED", female="#F08080")
+
+# Show the survival probability as a function of age and sex
+g = sns.lmplot(
+    x="age",
+    y="survived",
+    col="sex",
+    hue="sex",
+    data=df,
+    palette=pal,
+    y_jitter=0.02,
+    logistic=True,
+    truncate=False,
+)
+g.set(xlim=(0, 80), ylim=(-0.05, 1.05))
