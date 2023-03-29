065b52e38a6a33e68ca48ff67b368356ec2944c2
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-29 22:53:37 +0900

    Apply suggestions from code review

--- notebooks/notebooks/getfem-torsion.py
+++ notebooks/notebooks/getfem-torsion.py
@@ -40,5 +40,4 @@ from pyvista.examples import cells as example_cells, plot_cell
 grid = pv.CylinderStructured(
     radius=np.linspace(0.0, 1.0, 2), theta_resolution=4, z_resolution=2
 )
-block = pv.MultiBlock([grid]).combine(merge_points=True, tolerance=1.0e-15)
 example_cells.plot_cell(grid)
