2c9d46075b6a9c3342fd80d019b5618c7b8fc352
Tetsuo Koyama <tkoyama010@gmail.com>
2023-03-30 19:52:32 +0900

    Update codespace-install.sh

--- codespace-install.sh
+++ codespace-install.sh
@@ -1,4 +1,5 @@
 conda config --add channels conda-forge
 conda config --set channel_priority strict
 conda update conda
-conda install -y getfem==5.4.2
+conda install getfem==5.4.2 -y
+conda install pyvista==0.38.5 -y
