0f04f1bf717b8c9790356eae1c20404c1cc46726
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-03 00:18:36 +0900

    Update getfem-torsion.py

--- notebooks/notebooks/getfem-torsion.py
+++ notebooks/notebooks/getfem-torsion.py
@@ -62,3 +62,91 @@ grid = pv.CylinderStructured(
     radius=np.linspace(0.0, 50.0, 8), height=500.0, theta_resolution=16, z_resolution=25
 )
 example_cells.plot_cell(grid)
+
+# %%
+import getfem as gf
+import numpy as np
+import pyvista as pv
+
+pv.set_plot_theme("document")
+
+###############################################################################
+# Numerical parameters
+
+d = 0.100  # m
+L = 0.500  # m
+
+###############################################################################
+# Mesh generation.
+
+mesh = gf.Mesh("empty", 3)
+
+rhos = np.linspace(0.0, d / 2, 8 + 1)
+phis = np.linspace(0.0, 2.0 * np.pi, 16 + 1)
+zs = np.linspace(0.0, L, 25 + 1)
+
+rho_as = rhos[:-1]
+rho_bs = rhos[1:]
+phi_as = phis[:-1]
+phi_bs = phis[1:]
+z_as = zs[:-1]
+z_bs = zs[1:]
+
+for z_a, z_b in zip(z_as, z_bs):
+    for phi_a, phi_b in zip(phi_as, phi_bs):
+        mesh.add_convex(
+            gf.GeoTrans("GT_PRISM(3,1)"),
+            [
+                [
+                    0,
+                    d / 16 * 1 * np.cos(phi_a),
+                    d / 16 * 1 * np.cos(phi_b),
+                    0,
+                    d / 16 * 1 * np.cos(phi_a),
+                    d / 16 * 1 * np.cos(phi_b),
+                ],
+                [
+                    0,
+                    d / 16 * 1 * np.sin(phi_a),
+                    d / 16 * 1 * np.sin(phi_b),
+                    0,
+                    d / 16 * 1 * np.sin(phi_a),
+                    d / 16 * 1 * np.sin(phi_b),
+                ],
+                [z_a, z_a, z_a, z_b, z_b, z_b],
+            ],
+        )
+        for rho_a, rho_b in zip(rho_as, rho_bs):
+            mesh.add_convex(
+                gf.GeoTrans("GT_QK(3,1)"),
+                [
+                    [
+                        rho_a * np.cos(phi_a),
+                        rho_a * np.cos(phi_b),
+                        rho_b * np.cos(phi_a),
+                        rho_b * np.cos(phi_b),
+                        rho_a * np.cos(phi_a),
+                        rho_a * np.cos(phi_b),
+                        rho_b * np.cos(phi_a),
+                        rho_b * np.cos(phi_b),
+                    ],
+                    [
+                        rho_a * np.sin(phi_a),
+                        rho_a * np.sin(phi_b),
+                        rho_b * np.sin(phi_a),
+                        rho_b * np.sin(phi_b),
+                        rho_a * np.sin(phi_a),
+                        rho_a * np.sin(phi_b),
+                        rho_b * np.sin(phi_a),
+                        rho_b * np.sin(phi_b),
+                    ],
+                    [z_a, z_a, z_a, z_a, z_b, z_b, z_b, z_b],
+                ],
+            )
+
+mesh.export_to_vtk("mesh.vtk", "ascii")
+
+m = pv.read("mesh.vtk")
+plotter = pv.Plotter(off_screen=True)
+plotter.add_mesh(m, show_edges=True)
+plotter.show(screenshot='mesh.png')
