42ebe3a5230d202578673b15c7bddf93266611f4
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-09 09:24:47 +0900

    Add integration-getfem.py

---
+++ notebooks/notebooks/integration-getfem.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: py:percent,ipynb
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.5
+# ---
+
+# %% [markdown]
+# # GetFEMの動作確認
+#
+# このノートはGetFEMの動作確認を行うために作成したものです。
+
+# %%
+import getfem as gf
