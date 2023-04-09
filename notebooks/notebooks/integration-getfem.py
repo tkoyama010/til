25ee8e34d6d24c0935c410c9f775cdeaf0293f79
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-10 00:34:13 +0900

    Remove unused code

--- notebooks/notebooks/integration-getfem.py
+++ notebooks/notebooks/integration-getfem.py
@@ -24,42 +24,6 @@
 import getfem as gf
 import numpy as np
 import matplotlib.pyplot as plt
-import pyvista as pv
-
-pv.start_xvfb()
-pv.set_jupyter_backend("panel")
-
-# %% [markdown]
-# ## メッシュ生成
-#
-# GetFEM には，ここで説明するいくつかの制約のあるメッシュ機能があります．
-# ここではそれらを使用するつもりです．
-
-# %%
-mesh = gf.Mesh("cartesian", [0.0, 1.0])
-print(mesh)
-
-# %% [markdown]
-# ## メッシュの描画
-#
-# メッシュをプレビューし，その妥当性を制御するために，次の手順を使用します．
-
-# %%
-
-mesh.export_to_vtk("mesh.vtk", "ascii")
-m = pv.read("mesh.vtk")
-
-plotter = pv.Plotter()
-plotter.add_mesh(m)
-plotter.add_mesh(
-    pv.PolyData(m.points), color="red", point_size=10, render_points_as_spheres=True
-)
-plotter.show(cpos="xy")
-
-# %% [markdown]
-# ```{Tip}
-# 上に示したジオメトリはインタラクティブです。
-# ```
 
 # %% [markdown]
 # ## 積分法の定義
@@ -67,9 +31,6 @@ plotter.show(cpos="xy")
 # 定義するのは積分法 `mim` です．GetFEM にデフォルトの積分法はありません．
 # したがって，これは積分法を定義するためには必須です．
 # もちろん，積分法の次数は，選択された有限要素法に好都合な積分を行うため，十分に選定しなければなりません．
-
-# %%
-print([(1.0 - np.sqrt(1.0 / 3.0)) / 2.0, (1.0 + np.sqrt(1.0 / 3.0)) / 2.0])
 
 # %%
 im = gf.Integ("IM_GAUSS1D(1)")
