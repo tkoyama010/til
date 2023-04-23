24fe5bef3a197e3895426487e95f0e053ca7c664
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-23 14:17:38 +0900

    Add calculation

--- notebooks/notebooks/calcuate-area-lagrange-jacobian.py
+++ notebooks/notebooks/calcuate-area-lagrange-jacobian.py
@@ -10,22 +10,39 @@
 #       jupytext_version: 1.14.5
 # ---
 
-# %% [markdown]
-# ```math
-# x=N_1x_1+N_2x_2+N_3x_3+N_4x_4
-# ```
-# ```math
-# y=N_1y_1+N_2y_2+N_3y_3+N_4y_4
-# ```
-# ```math
-# N_1=\frac{1}{4}\left(1-\xi \right)\left(1-\eta \right)
-# ```
-# ```math
-# N_2=\frac{1}{4}\left(1+\xi \right)\left(1-\eta \right)
-# ```
-# ```math
-# N_3=\frac{1}{4}\left(1+\xi \right)\left(1+\eta \right)
-# ```
-# ```math
-# N_4=\frac{1}{4}\left(1-\xi \right)\left(1+\eta \right)
-# ```
+# %% [code]
+import sympy as sym
+from myst_nb import glue
+
+xi, eta = sym.symbols("xi eta")
+
+N1 = 1.0 / 4.0 * (1.0 - xi) * (1.0 + eta)
+glue("N1", N1)
+N2 = 1.0 / 4.0 * (1.0 - xi) * (1.0 + eta)
+glue("N2", N2)
+N3 = 1.0 / 4.0 * (1.0 - xi) * (1.0 + eta)
+glue("N3", N3)
+N4 = 1.0 / 4.0 * (1.0 - xi) * (1.0 + eta)
+glue("N4", N4)
+
+# %% [code]
+x1, x2, x3, x4 = sym.symbols("x_{1} x_{2} x_{3} x{4}")
+y1, y2, y3, y4 = sym.symbols("y_{1} y_{2} y_{3} y{4}")
+
+# %% [code]
+
+x = N1 * x1 + N2 * x2 + N3 * x3 + N4 * x4
+glue("x", x)
+y = N1 * y1 + N2 * y2 + N3 * y3 + N4 * y4
+glue("y", y)
+
+# %% [code]
+
+J = sym.Matrix(
+    [[sym.diff(x, xi), sym.diff(y, xi)], [sym.diff(x, eta), sym.diff(y, eta)]]
+)
+glue("J", J)
+
+# %% [code]
+glue("|J|", sym.diff(x, xi) * sym.diff(y, eta) - sym.diff(y, xi) * sym.diff(x, eta))
+
