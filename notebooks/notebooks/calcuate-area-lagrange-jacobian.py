f0a76a64a65f7851f12133a1815e867520dcd365
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-23 18:33:04 +0900

    Update calcuate-area-lagrange-jacobian.py

--- notebooks/notebooks/calcuate-area-lagrange-jacobian.py
+++ notebooks/notebooks/calcuate-area-lagrange-jacobian.py
@@ -44,5 +44,5 @@ J = sym.Matrix(
 glue("J", J)
 
 # %% [code]
-detJ= sym.diff(x, xi) * sym.diff(y, eta) - sym.diff(y, xi) * sym.diff(x, eta
+detJ= sym.diff(x, xi) * sym.diff(y, eta) - sym.diff(y, xi) * sym.diff(x, eta)
 glue("|J|", detJ)
