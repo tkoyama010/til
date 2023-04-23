08c161da8db5848f33462aadcb904718d123c07d
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-23 13:28:51 +0900

    Update calcuate-area-lagrange-jacobian.py

--- notebooks/notebooks/calcuate-area-lagrange-jacobian.py
+++ notebooks/notebooks/calcuate-area-lagrange-jacobian.py
@@ -1,1 +1,13 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: py:percent,ipynb
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.5
+# ---
 
+# %% [markdown]
