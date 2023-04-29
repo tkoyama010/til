Fix calculation

notebooks/notebooks/torsion-getfem.py
@@ -269,11 +269,11 @@ md.add_initialized_data("H2", [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]
 md.add_generalized_Dirichlet_condition_with_multipliers(
     mim, "u", mfu, BOTTOM_BOUND, "r2", "H2"
 )
-md.add_linear_term(
-    mim,
-    str(16.0 * T / np.pi / d**3 / (d / 2.0)) + "*" + "[-X(2), X(1), 0.0].Test_u",
-    TOP_BOUND,
-)
+
+tau = 16.0 * T / np.pi / d**3
+radius = d / 2.0
+
+md.add_linear_term(mim, str(tau / radius) + "*" + "[-X(2), X(1), 0.0].Test_u", TOP_BOUND)
 
 # %% [markdown]
 # ## モデルの求解