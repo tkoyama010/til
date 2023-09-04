1f56ec0395f06acac713837deed572cb96466f60
Tetsuo Koyama <tkoyama010@gmail.com>
2023-09-04 23:09:01 +0900

    :memo: Project Euler #2: Even Fibonacci numbers

---
+++ notebooks/notebooks/project-euler.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: py:percent,ipynb
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.5
+# ---
+
+# %% [markdown]
+# # Project Euler #2: Even Fibonacci numbers
+#
+# Fibonacci数列は3項ごとに偶数の項となる。
+
+# %% [markdown]
+# 
+# $$
+# \frac{1}{\sqrt{5}}\left\{\left(\frac{1+\sqrt{5}}{2}\right)^n-\left(\frac{1-\sqrt{5}}{2}\right)^n\right\}<N
+# $$
+
+# %%
+print("Hello World!")
