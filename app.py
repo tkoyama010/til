f9d4bd00a8a906f27a9675c312f9e0e08059465f
Tetsuo Koyama <tkoyama010@gmail.com>
2024-04-20 13:16:11 +0900

    Update app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# GetFEMによる丸棒のねじり解析")
+st.markdown("本記事は [Salome-Meca2019による丸棒のねじり解析](https://qiita.com/femlabo-toyo/items/b28ef117ef003ad652f0) をGetFEMで再現したものです．")
